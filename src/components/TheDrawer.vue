f1944da9623a5051f2d80f9852bedcbf374a6de2
Kenert Karu <karukenert@gmail.com>
2022-10-03 21:43:49 +0300

    setup() - minor changes

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import {useAuth} from "../composables/useAuth";
+import {ref} from "vue";
 
+const { user, client } = useAuth();
+const budgets = ref<Record<string, unknown>[]>([{a:1}, {a:2}, {a:3}]);
 </script>
 
 <template>
   <v-navigation-drawer app>
-    drawer
+    <v-list>
+      <v-list-item link v-for="i in budgets" >
+        {{ i }}
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
